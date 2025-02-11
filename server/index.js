be48a64364fcc75e020d2b1d8e026ba7d330d387
jxho1 <jxho1@uw.edu>
2025-02-10 20:27:37 -0800

    Updated backend: created friends system

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const corsConfig = require("./src/middleware/corsConfig");
 const authRoutes = require("./src/routes/authRoutes");
 const userRoutes = require("./src/routes/usersRoutes");
+const friendsRoutes = require("./src/routes/friendsRoutes");
 
 require('dotenv').config();
 
@@ -18,6 +19,7 @@ app.use(corsConfig);
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/friends", friendsRoutes);
 
 // Root route
 app.get("/", (req, res) => {
