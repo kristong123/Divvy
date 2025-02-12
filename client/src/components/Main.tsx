5616e3d856844f0c7e2d2d8911e8f2d3a5d79151
Kris Tong <kristong@Kriss-MacBook-Air.local>
2025-02-12 13:51:58 -0800

    Developing sidebar and dashboard

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -3,9 +3,9 @@ import Sidebar from './Sidebar';
 
 const Dashboard: React.FC = () => {
   return (
-    <div className="w-screen h-screen bg-white">
+    <div className="row w-screen h-screen bg-white">
       <Sidebar/>
-      <h1>Dashboard</h1>
+      <h1 className='text-5xl font-bold text-dark1'>Divvy</h1>
     </div>
   )
 };
--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { UserRound } from 'lucide-react';
+import { UserRound, UserRoundPlus, UsersRound, Bell } from 'lucide-react';
 import { useSelector } from 'react-redux'; // Import useSelector
 import { RootState } from '../store/store'; // Import RootState to type the state
 
@@ -7,21 +7,28 @@ const Sidebar: React.FC = () => {
   const username = useSelector((state: RootState) => state.user.username);
 
   return (
-    <div>
-      <div className="col justify-between p-4">
-        <div className="col h-fit">
-          <div>
-            <div className="flex bg-gradient-to-br from-dark2 to-light1 rounded-full w-16 h-16">
-              <UserRound className="m-auto w-12 h-12"/>
+    <div className='row'>
+      <div className='w-60'>
+        <div className='col h-fit'>
+          <div className='row p-4'>
+            <div className='flex rounded-full w-16 h-16 bg-gradient-to-br from-dark2 to-light1'>
+              <UserRound className='m-auto h-12 w-12'/>
             </div>
-            {/* Display the username from the Redux store */}
-            <p>{username || 'Guest'}</p> {/* Fallback to 'Guest' if username is null */}
+            <p className='ml-4 my-auto text-2xl font-bold text-black'>
+              {username || 'Guest'}
+            </p> 
           </div>
-          <div>
-            {/* Other sidebar content */}
+          <div className='h-0.5 bg-gradient-to-l from-black to-white'></div>
+          <div className='row py-3'>
+            <Bell className='stroke-black m-auto h-10 w-10'/>
+            <UsersRound className='stroke-black m-auto h-10 w-10'/>
+            <UserRoundPlus className='stroke-black m-auto h-10 w-10'/>
           </div>
         </div>
       </div>
+
+      {/* Divider */}
+      <div className='w-0.5 bg-gradient-to-b from-black to-white'></div>
     </div>
   );
 };
