ccaebaded44633cc56060fb01d6380e92a21412c
Kris Tong <kristong@Kriss-MacBook-Air.local>
2025-02-13 15:26:42 -0800

    Updated cors urls

--- server/src/middleware/corsConfig.js
+++ server/src/middleware/corsConfig.js
@@ -1,7 +1,11 @@
 const cors = require("cors");
 
 const corsConfig = cors({
-  origin: ["http://localhost:3000", "http://localhost:5173"],
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://divvy-chi.vercel.app",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
